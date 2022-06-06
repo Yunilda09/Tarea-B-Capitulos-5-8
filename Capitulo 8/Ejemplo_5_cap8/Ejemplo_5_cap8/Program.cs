2edb3742255d618e6eea60fc9be088f36432c645
Yunilda Justo <105021692+Yunilda09@users.noreply.github.com>
2022-06-06 14:20:21 -0400

    Add files via upload

---
+++ Capitulo 8/Ejemplo_5_cap8/Ejemplo_5_cap8/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TareasProgramacionAplicada.Tarea2
+{
+    /// 5 Hacer un programa que le solicite al usuario dos cadenas y luego las muestre en orden alfabético.
+    
+    class Cadenas
+    {
+
+        public static void Main(string[] args)
+        {
+            string cadena1, cadena2;
+            int comparacion;
+
+            Console.WriteLine("Digite la cadena 1");
+            cadena1 = Console.ReadLine();
+            Console.WriteLine("Digite la cadena 2");
+            cadena2 = Console.ReadLine();
+
+            comparacion = String.Compare(cadena1, cadena2);
+            Console.Clear();
+            if (comparacion > 0)
+                Console.WriteLine(cadena2 + "  " + cadena1);
+            else
+                Console.WriteLine(cadena1 + "  " + cadena2);
+
+        }
+    }
+}
---
+++ Capitulo 8/Ejercico_3_cap8/Ejercicio_3_cap8/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo_3_cap8
+{
+    /// 3 Hacer un programa que muestre la hora del día en formato AM/FM seguida del año, el día y el mes actual.
+   
+    class FechaHora
+    {
+   
+        public static void Main(string[] args)
+        {
+            String formato;
+
+            formato = String.Format("La hora y fecha es: {0:hh tt yyyy dd M}", DateTime.Now);
+            Console.WriteLine(formato);
+        }
+
+    }
+}
